eef797d077c8540c0d83fa8916ca1c295bcfaeae
Ravi1470 <vk8580318@gmail.com>
2024-08-02 10:55:52 +0530

    added two files

--- Aug2/Q1.java
+++ Aug2/Q1.java
@@ -1,5 +1,22 @@
+
+// write a java program to print elements thoose who are not having vowels.
+
 public class Q1 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String[] s = {"abc", "hi", "sdf", "xyz", "efg"};
+
+        for (String i : s) {
+            boolean flag = true;
+            for (int j = 0; j < i.length(); j++) {
+                if (i.charAt(j) == 'a' || i.charAt(j) == 'e' || i.charAt(j) == 'i' || i.charAt(j) == 'o'
+                        || i.charAt(j) == 'u') {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                System.out.println(i);
+            }
+        }
     }
 }
